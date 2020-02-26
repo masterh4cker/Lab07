84c2719b4d88876b06b74fddb0151e7016806e92
masterh4cker <tabathafoes@gmail.com>
2020-02-26 11:13:23 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app",
     template: `<div class="bg-success p-2 text-center text-white"> 
-    This is SportsStore 
+    This is SportsStore
     </div>`
 })
 export class AppComponent { }
