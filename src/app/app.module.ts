272426a2b21af46d14a9423c347cadb7c7452a1b
masterh4cker <tabathafoes@gmail.com>
2020-02-27 08:59:29 -0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-//import { AppComponent } from "./app.component";
-//import { StoreModule } from "./store/store.module";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "./store/store.module";
 
 
 @NgModule({
-   //imports: [BrowserModule, StoreModule],
-  //  declarations: [AppComponent],
-   // bootstrap: [AppComponent]
+imports: [BrowserModule, StoreModule],
+declarations: [AppComponent],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
 
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { ModelModule } from "../model/model.module";
-import { StoreComponent } from "./store.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ModelModule } from '../model/model.module';
+import { StoreComponent } from './store.component';
 
 
 @NgModule({
