429f1475cd75d4b2e3dfe5b88f8100ec8874a345
masterh4cker <tabathafoes@gmail.com>
2020-02-27 10:33:15 -0700

    2 files modified

--- src/app/store/store.componet.ts
+++ src/app/store/store.componet.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Product } from '../model/product.model';
-import { ProductRepository } from '../model/product.repository';
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 
 @Component({
     selector: "store",
