59b093c4614268c67543f93e8b36a616f4c36c8f
masterh4cker <tabathafoes@gmail.com>
2020-02-27 10:57:32 -0700

    2 files modified

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
-import { StoreComponent } from "./store.componet";
+import { StoreComponent } from "./store.component";
 
 
 @NgModule({
